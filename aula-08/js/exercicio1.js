26fef561eb7b1d9b77ef262bac6c1cf620d47c55
everlen <lucasrufinomaiellaro@gmail.com>
2019-11-06 22:01:39 -0300

    adicionando aula 8

---
+++ aula-08/js/exercicio1.js
@@ -0,0 +1,9 @@
+let degrau = Number(prompt("Insira quantos degraus voce quer que a sua escada tenha"));
+let material = prompt("Insira qual material voce quer usar para construir sua esscada");
+
+for(let i = 0; i < degrau; i++){
+    for(quantidadeMaterial = 0; quantidadeMaterial < degrau; quantidadeMaterial++){
+        console.log(material[quantidadeMaterial]);
+    }
+   
+}
---
+++ aula-08/js/script.js
@@ -0,0 +1,12 @@
+// let animes = ['Death Note', 'Boku no Hero', 'Dragon Ball', 'Cavaleiros do Zodíaco', 'Naruto', 
+// 'Super Onze', 'Nazo no Kanojo']; //lista
+// let i = 0;
+
+// while(i < animes.length){
+//     console.log(animes[i]);
+//     i++;
+// }
+
+// for(let e = 0; e < animes.length; e++){
+//     console.log(animes[e]);
+// }
